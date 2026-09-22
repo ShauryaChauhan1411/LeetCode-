0594a424ded985a7f61ea61054c24e35c88abeb8
Shaurya Vardhan Singh Chauhan <chauhanshaurya1411@gmail.com>
2026-09-22 15:44:13 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (94.46%) - LeetSync

---
+++ 4353-largest-integer-with-given-digit-sum/largest-integer-with-given-digit-sum.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int largestInteger(int n, int s) {
+        int ans=0;
+        while(n>0){
+            if(s>=9){
+                ans=ans*10+9;
+                s-=9;
+            }
+            else
+            {
+                ans=ans*10+s;
+                s=0;
+            }
+            n--;
+        }
+        if(s!=0)
+        return -1;
+        return ans;
+    }
+}
